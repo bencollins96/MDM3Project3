53ac2ca9ceb35eda88229bc5a948f68fe426c3dc
Ben Collins <bc14714@my.bristo.ac.uk>
2017-02-09 22:28:12 +0000

    Very little changed...

--- preprocessing.m
+++ preprocessing.m
@@ -15,11 +15,12 @@ for i= 1:length(AllData)-1
     result = cellfun(@str2num, profile, 'UniformOutput', false);
     result = cell2mat(result);
     profile_values{i} = result;
+    
 end
 
 for i = 1:length(AllData)-1
     hold on 
-    plot(profile_values{i})
+    plot(profile_values{i});
 end
 
 end
